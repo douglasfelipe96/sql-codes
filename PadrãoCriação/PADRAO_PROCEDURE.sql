c94562b7ca0e208a13d304c2d4f65531cc100609
Douglas Felipe <105475081+douglasfelipe96@users.noreply.github.com>
2023-09-14 12:27:34 -0300

    refactor: padrões de criação revisado

---
+++ PadrãoCriação/PADRAO_PROCEDURE.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE PROCEDURE HCD_NOME_DA_FUNÇÃO ('PARÂMETROS')
+AS
+
+/*<DS_SCRIPT>
+ DESCRIÇÃO...: Function que retorna os horários dos itens do lote da prescrição
+ RESPONSAVEL.: DOUGLAS FELIPE (douglas.felipe)
+ DATA........: 09/11/2022
+ APLICAÇÃO...: TASY
+ ATUALIZAÇÃO...:
+    -- EXEMPLO:Atualizado em 07/01/22 por Douglas F: Adicionado filtro para buscar o bloqueio também pelo horário caso informado
+ OBSERVAÇÕES...:  
+    -- Douglas F: Parâmetro NR_SEQ_SUPERIOR_P utilizado apenas para os componentes dos médicamentos, sendo assim para o medicamento principal não necessita a passagem do mesmo.
+    -- Douglas F: Parâmetro IE_OPCAO_P pode ser 'A' - Agrupados ou 'N' - Normal.
+</DS_SCRIPT>*/
+
+BEGIN
+
+
+END HCD_NOME_DA_FUNÇÃO;
